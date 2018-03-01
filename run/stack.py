d4f5a26624f8e7206395ca759a45c5367c5bf204
tuckerhaydon <tuckerhaydon@gmail.com>
2018-02-28 18:18:58 -0600

    Updated config files

--- run/stack.py
+++ run/stack.py
@@ -101,33 +101,36 @@ launch_tmux_window(window_name)
 quarter_window(window_name)
 
 # GPS Kalman Filter
-window_command = ("roslaunch gps_kf gpsQuad.launch" +
-    " quad_namespace:=" + QUAD_NAMESPACE)
-run_command_in_window(window_name, window_command, 0)
-
-# PX4 Control
-window_command = ("roslaunch px4_control stack.launch ns:=" + QUAD_NAMESPACE)
-run_command_in_window(window_name, window_command, 1)
+# window_command = ("roslaunch gps_kf gpsQuad.launch" +
+#     " quad_namespace:=" + QUAD_NAMESPACE)
+# run_command_in_window(window_name, window_command, 0)
 
 # Waypoint control
-window_command = ("roslaunch waypoint_control stack.launch")
-run_command_in_window(window_name, window_command, 2)
+# window_command = ("roslaunch waypoint_control stack.launch")
+# run_command_in_window(window_name, window_command, 2)
 
 
 
 ## Start Snapdragon files
 window_name = "SNAP"
 launch_tmux_window(window_name)
-split_window(window_name, '-v')
+quarter_window(window_name)
 
 # PX4
-window_command = ("ssh -t " +
-    SNAPDRAGON_USERNAME + 
-    "@" +
-    SNAPDRAGON_IP + 
-    " \'echo /home/linaro/px4 /home/linaro/mainapp.config | bash -i\'")
-run_command_in_window(window_name, window_command, 0)
+# window_command = ("ssh -t " +
+#     SNAPDRAGON_USERNAME + 
+#     "@" +
+#     SNAPDRAGON_IP + 
+#     " \'/home/linaro/px4 /home/linaro/mainapp.config\'")
+# run_command_in_window(window_name, window_command, 0)
 
+# PX4 Control
+# window_command = ("ssh -t " +
+#     SNAPDRAGON_USERNAME + 
+#     "@" +
+#     SNAPDRAGON_IP + 
+#     " \'echo roslaunch px4_control stack.launch ns:=" + QUAD_NAMESPACE + " | bash -i\'")
+# run_command_in_window(window_name, window_command, 1)
 
 # Launch Camera
 if(USE_CAMERA == True):
@@ -136,7 +139,7 @@ if(USE_CAMERA == True):
         "@" +
         SNAPDRAGON_IP + 
         " \'echo roslaunch snap_cam main.launch | bash -i\'")
-    run_command_in_window(window_name, window_command, 1)
+    run_command_in_window(window_name, window_command, 2)
 
 
 
