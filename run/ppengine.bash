8749356e3d5c9abbc2cf29f717bc5b60749f5def
tuckerhaydon <tuckerhaydon@gmail.com>
2018-04-02 17:01:28 -0500

    update config files

--- run/ppengine.bash
+++ run/ppengine.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-REFNET_GBX="refnet"
+INPUT_GBX="pprx.gbx"
 QUAD_GBX="phoenix"
 A2D="A2D.cfg"
 SBRTK="SBRTK.cfg"
 
-rosrun ppengineros ppengineros --ref $REFNET_GBX --rov $QUAD_GBX --out $QUAD_GBX --config $A2D --config $SBRTK
+rosrun ppengineros ppengineros --in $INPUT_GBX --out $QUAD_GBX --config $A2D --config $SBRTK
