e1db8e380da00afbaf4b892346c4fafb5d306a39
James Yoder <james@jyoder.com>
2018-05-10 17:52:05 -0500

    update scripts

---
+++ install_scripts/ros_env.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export ROS_IP=$(hostname -I)
+export ROS_MASTER_URI=http://192.168.1.102:11311/
+
+source "$DIR/../src/build/devel/setup.bash"
