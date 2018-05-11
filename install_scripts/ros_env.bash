ebb9a0cba6799762b60202ed892bcf3837c8d7d8
James Yoder <james@jyoder.com>
2018-05-11 17:07:30 -0500

    Dont force external ros master

--- install_scripts/ros_env.bash
+++ install_scripts/ros_env.bash
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export ROS_IP=$(hostname -I)
-export ROS_MASTER_URI=http://192.168.1.102:11311/
+#export ROS_MASTER_URI=http://192.168.1.102:11311/
 
 source "$DIR/../src/build/devel/setup.bash"
